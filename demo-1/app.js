07d4f0d9f7d19efce0fa2da74a6fdfee625129a7
LiangCanLun <452562076@qq.com>
2015-11-30 14:04:01 +0800

    add ui route

--- demo-1/app.js
+++ demo-1/app.js
@@ -2,5 +2,23 @@
 (function (angular) {
 	"use strict";
 
-	angular.module('bookStoreApp', []);
+	angular.module('bookStoreApp', []).config(['$stateProvider', function ($stateProvider) {
+		$stateProvider
+			.state('bookstore', {
+				url: '/',
+				controller: 'bookMainController',
+				template: 'partials/book-main-view.html'
+			})
+			.state('bookstoreList', {
+				url: 'bookstore/list',
+				controller: 'bookListController',
+				template: 'partials/book-list-view.html'
+			})
+			.state('bookstoreDetail', {
+				url: 'bookstore/detail',
+				controller: 'bookDetailController',
+				template: 'partials/book-detail-view.html'
+			});
+
+	}]);
 }(angular));
