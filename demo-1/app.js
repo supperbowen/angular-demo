d70a6513621f7bfd59437f7910dc585b0c2220d5
bowen liang <bowenliang@outlook.com>
2015-12-01 23:09:59 +0800

    code optimize

--- demo-1/app.js
+++ demo-1/app.js
@@ -1,24 +1,26 @@
 /*global angular*/
 (function (angular) {
-	"use strict";
+    "use strict";
 
-	angular.module('bookStoreApp', []).config(['$stateProvider', function ($stateProvider) {
-		$stateProvider
-			.state('bookstore', {
-				url: 'bookstore/',
-				controller: 'bookStoreMainController',
-				template: './partials/book-main-view.html'
-			})
-			.state('bookstoreList', {
-				url: 'bookstore/list',
-				controller: 'bookStoreListController',
-				template: './partials/book-list-view.html'
-			})
-			.state('bookstoreDetail', {
-				url: 'bookstore/detail',
-				controller: 'bookStoreDetailController',
-				template: './partials/book-detail-view.html'
-			});
+    angular.module('bookStoreApp', ['ui.router']).config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.when("", "/bookstore/list");
 
-	}]);
+        $stateProvider
+            .state('bookstore', {
+                url: 'bookstore/',
+                controller: 'bookStoreMainController',
+                template: './partials/book-main-view.html'
+            })
+            .state('bookstoreList', {
+                url: 'bookstore/list',
+                controller: 'bookStoreListController',
+                template: './partials/book-list-view.html'
+            })
+            .state('bookstoreDetail', {
+                url: 'bookstore/detail',
+                controller: 'bookStoreDetailController',
+                template: './partials/book-detail-view.html'
+            });
+
+    }]);
 }(angular));
